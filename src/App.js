c538d888d7d2bdbbcaf271a9339402fd9f4ae712
Muniba Ahmed <mahmed93@berkeley.edu>
2022-03-03 14:34:06 -0800

    created new folders & set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import About from "./Components/About";
+
+function App() {
+  return (
+    <main className="main">
+      <About />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+//in every component we are creating a new function
+const About = () => {
+  return (
+    <div>
+      <h1> Munibasayshi</h1>
+    </div>
+  );
+};
+
+export default About;
